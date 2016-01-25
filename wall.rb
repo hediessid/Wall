4229e1ab5bb780010672917203f5ebeef39230fd
hediessid <essidhedi7000@gmail.com>
2016-01-25 04:49:12 -0500

    compare_all_rows

--- wall.rb
+++ wall.rb
@@ -25,42 +25,39 @@ class Wall
   end
 
   def compare_superior_rows(all_rows, row_level, level, height, count=0)
-     # binding.pry
-    while (level < height)
-      if (level <height)
-       result = compare_all_rows(all_rows,result)[0]
-       compare_all_rows(all_rows,result)
-        level += 1
+     
+    all_rows.each do |row|
+      if ((level == 2 )&&(height==2))
+        return compare_all_rows(all_rows,row_level).count
       elsif (level == height)
-        count+= compare_all_rows(all_rows,row_level)[0].count
+        count+= compare_all_rows(all_rows,row_level).count
+       else
+        level += 1
+        row_level = compare_all_rows(all_rows,row_level)
+        compare_superior_rows(all_rows,row_level,level,height) 
       end    
     end
       puts count  
   end
 
-  def compare_all_rows(all_rows,rows,count=0)
-    hash_all_comparaison={}
-    rows.each do |row|
-      all_rows.each do |row_to_compare|
+  def compare_all_rows(all_rows,rows)
+    array=[]
+    all_rows.each do |row|
+      rows.each do |row_to_compare|
         if (row - row_to_compare)== row
-          hash_all_comparaison[row]=row_to_compare
+          array.push(row)
         end     
       end
-      count+=hash_all_comparaison[row].count
     end
-    return hash_all_comparaison,count
+    return array
   end 
-
   def total_count(width, height)
     level = 2
     #binding.pry
     rows= all_rows = all_possibilities([3,4.5],[],[0]) 
-    row_level = compare_all_rows(all_rows,rows)
     puts all_rows.count if height==1
-    puts row_level[1] if height==2
-    if height > 2
-
-      puts compare_superior_rows(all_rows,row_level,level,height)
+    if height >= 2
+      puts compare_superior_rows(all_rows,rows,level,height)
     end                        
   end
 end 
