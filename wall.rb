1068342c58fb0cde9426015f9c9430a04f0cd647
hediessid <essidhedi7000@gmail.com>
2016-01-25 19:21:16 -0500

    fixed

--- wall.rb
+++ wall.rb
@@ -1,5 +1,6 @@
 require 'pry'
 class Wall
+    
   def initialize(width,height,bigBrick,smallBrick)
     @width = width
     @height = height
@@ -24,47 +25,64 @@ class Wall
     end
   end
   # need to use the first array that we create from comparaison 
-  def compare_superior_rows(all_rows, row_level, level, height, count=0)
-     binding.pry
-    while (level <= height)
-      if ((level == 2 )&&(height==2))
-        return compare_all_rows(all_rows,row_level).count
-      elsif (level == height)
-        count+= compare_all_rows(all_rows,row_level).count
-       else
-        level += 1
-        row_level = compare_all_rows(all_rows,row_level)
-        compare_superior_rows(all_rows,row_level,level,height) 
-      end    
+  def compare_superior_rows(all_row, row_level, level, height)
+    hash={}
+    binding.pry
+    while (level <= height)  
+      if (level==height)
+        puts hash
+      else
+        hash.values.flatten(1).each do |array|
+          if ((hash.empty? == true) || (hash.has_key?(array)==false))
+             hash[level]=[row_level[array]]
+             binding.pry
+          else
+            binding.pry
+            hash[level].push([row_level[array]])           
+          end        
+        end 
+
+      end 
+    level+=1
     end
-      puts count  
   end
 
   def compare_all_rows(all_rows,rows)
-    array=[]
+    hash={}
     all_rows.each do |row|
       rows.each do |row_to_compare|
         if (row - row_to_compare)== row
-          array.push(row)
+          if ((hash.empty? == true) || (hash.has_key?(row)==false)) 
+            hash[row]=[row_to_compare]
+          else
+            hash[row].push(row_to_compare) 
+          end  
         end     
       end
-    end
-    return array
+    end 
+    return hash
   end 
 
   def total_count(width, height)
-    level = 1
-    #binding.pry
-    rows= all_rows = all_possibilities([3,4.5],[],[0]) 
-    puts all_rows.count if height==1
-    if height >= 2
-      puts compare_superior_rows(all_rows,rows,level,height)
-    end                        
+    level = 2
+    count = 0
+    rows = all_rows = all_possibilities([3,4.5],[],[0])
+    all_rows_level = compare_all_rows(all_rows, rows)
+    all_rows_level1 = all_rows_level.values.flatten(1)
+
+    all_rows_level1.each do |value|
+      count+= value.count
+    end 
+    rows.count if height == 1
+    puts all_rows_level1.count if height == 2 
+    if height > 2 
+      compare_superior_rows(rows, all_rows_level, level, height)
+    end                     
   end
 end 
 
-wall = Wall.new(27,5,4.5,3)
-wall.total_count(27,5)
+wall = Wall.new(9,3,4.5,3)
+wall.total_count(9,3)
 
 
 
